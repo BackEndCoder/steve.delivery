858496e2ed12b55817b52a7907f460863767a4e6
BackEndCoder <steve.coverdale@gmail.com>
2021-01-25 09:05:46 +0000

    corect spelling

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 @section('content')
 
       <h1 class="mt-5">Steve Delivery</h1>
-      <p class="lead">I am a self employed <b>Bicycle Messenger</b> working in <b>Central London</b>. You can <a href="/hire-me">hire me</a> for bookings. The companies below have prevoiusly used my service :-</p>
+      <p class="lead">I am a self employed <b>Bicycle Messenger</b> working in <b>Central London</b>. You can <a href="/hire-me">hire me</a> for tset bookings. The companies below have previously used my service :-</p>
     <div class="row center" style="height: 250px;
     line-height: 250px; max-height: 250px;">
         <div class="col-3">
